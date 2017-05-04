7833356f1fc9b7f6abf6fadb999cca4ae18a263f
Steven Warmelink <stevenwarmelink@gmail.com>
2017-05-04 11:19:28 +0200

    added horizontal pixeldensity

--- pixelDensity/pixelDensity.py
+++ pixelDensity/pixelDensity.py
@@ -6,6 +6,8 @@ img = cv2.imread('example.pgm')
 
 sum = 0
 
+
+
 a = []
 
 height, width, channels = img.shape
@@ -19,5 +21,24 @@ for j in range(1,width):
 			sum +=1
 	a.append(sum);
 
+plt.subplot(2,1,1)
 plt.plot(a);
+plt.title('Vertical pixel density')
+
+a = []
+
+for j in range(1,height):
+
+	sum = 0;
+	for i in range (1,width):
+		px = img[j,i][0]
+		if px == 0:
+			sum +=1
+	a.append(sum);
+
+
+plt.subplot(2,1,2)
+plt.plot(a);
+plt.title('Horizontal pixel density')
 plt.show();
+
