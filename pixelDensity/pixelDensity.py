6b2b7d606f079b7c0a5a3e2d13a8bb03bbffe2be
Steven Warmelink <stevenwarmelink@gmail.com>
2017-05-04 12:15:22 +0200

    image overlay pixeldensities

--- pixelDensity/pixelDensity.py
+++ pixelDensity/pixelDensity.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('example.pgm')
+img = cv2.imread('example2.pgm')
 
 sum = 0
 
 
 
-a = []
+vertical_pixel_density = []
 
 height, width, channels = img.shape
 
@@ -19,13 +19,32 @@ for j in range(1,width):
 		px = img[i,j][0]
 		if px == 0:
 			sum +=1
-	a.append(sum);
+	vertical_pixel_density.append(sum);
+
+seperators = []
+
+prev_loc = 0
+
+for i in range (1,len(vertical_pixel_density)):
+	if not ((i - prev_loc) < 75):
+		if vertical_pixel_density[i] < 4:
+			prev_loc = i
+			seperators.append(i)
+
+print seperators
 
 plt.subplot(2,1,1)
-plt.plot(a);
+plt.plot(vertical_pixel_density);
 plt.title('Vertical pixel density')
 
-a = []
+for i in range(0,len(seperators)):
+	x = seperators[i]
+	plt.plot([x,x],[0,height])
+
+
+horizontal_pixel_density = []
+
+
 
 for j in range(1,height):
 
@@ -34,11 +53,20 @@ for j in range(1,height):
 		px = img[j,i][0]
 		if px == 0:
 			sum +=1
-	a.append(sum);
+	horizontal_pixel_density.append(sum);
 
 
 plt.subplot(2,1,2)
-plt.plot(a);
+plt.plot(horizontal_pixel_density);
 plt.title('Horizontal pixel density')
 plt.show();
+
+for i in range(0,len(seperators)):
+	for j in range(0,height-1):
+		xval = seperators[i]
+		img[j][xval] = [0,0,0]
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
