6fcab380fa1494446185304ed68e4f3bf937e664
Steven Warmelink <stevenwarmelink@gmail.com>
2017-05-08 14:03:46 +0200

    added splitting of image, rotation and cropping

--- pixelDensity/pixelDensity.py
+++ pixelDensity/pixelDensity.py
@@ -15,8 +15,7 @@ def rotateImage(img):
 	resultImage = img
 	maxheight = 0
 
-	for i in np.arange(-5,5,0.2):
-	#img = cv2.copyMakeBorder(img,100,100,100,100,cv2.BORDER_CONSTANT,value=[255,255,255])
+	for i in np.arange(-2,2,0.2):
 		height, width = img.shape
 		M = cv2.getRotationMatrix2D((width/2,height/2),i,1)
 		rotatedImage = cv2.warpAffine(img,M,(width,height))
@@ -31,10 +30,45 @@ def rotateImage(img):
 			maxheight = tempmax
 			resultImage = rotatedImage
 
-
-
-
 	return np.asarray(resultImage)
+
+def cropImage(img, h_thresh, v_thresh, h_dens, v_dens):
+	height, width = img.shape
+
+	vborders = []
+	hborders = []
+
+	subimg = img
+
+	for i in range (0, width-1):
+		if v_dens[i] > v_thresh:
+			vborders.append(i)
+	
+	for j in range (0, height-1):
+		if h_dens[j] > h_thresh:
+			hborders.append(j)
+
+	if len(hborders) > 0:
+		if (max(hborders) - min(hborders)) > 100:
+			subimg = subimg[min(hborders):max(hborders),0:width-1]
+		else:
+			if max(hborders) > height/1.5:
+				subimg = subimg[0:min(hborders),0:width-1]	
+			else: 
+				subimg = subimg[max(hborders):height-1,0:width-1]
+
+	if len(vborders) > 0:
+		if (max(vborders) - min(vborders)) > 100:
+			subimg = subimg[0:height-1,min(vborders):max(vborders)]
+		else:
+			if max(vborders) > width/2:
+				subimg = subimg[0:height-1,0:min(vborders)]	
+			else: 
+				subimg = subimg[0:height-1,max(vborders):width-1]
+
+	return np.asarray(subimg)
+
+
 
 def calcVerPixelDensity(img):
 	vertical_pixel_density = []
@@ -66,16 +100,17 @@ def calcHorPixelDensity(img):
 
 	return horizontal_pixel_density
 
-def CalcSeperators(vertical_pixel_density,minBoxWidth,threshold):
+def CalcSeperators(vertical_pixel_density,minBoxWidth,threshold,padding):
 	seperators = []
 
 	prev_loc = 0
 
-	for i in range (0,len(vertical_pixel_density)):
+	for i in range (0,len(vertical_pixel_density)-1):
 		if not ((i - prev_loc) < minBoxWidth):
 			if vertical_pixel_density[i] < threshold:
 				prev_loc = i
-				seperators.append(i)
+				if(i+padding) < len(vertical_pixel_density)-1:
+					seperators.append(i+padding)
 
 	return seperators
 
@@ -84,13 +119,34 @@ def drawSeperators(img, seperators, padding):
 
 	for i in range(0,len(seperators)):
 		for j in range(0,height-1):
-			xval = seperators[i] + padding
+			xval = seperators[i]
 			if xval < width:
 				img[j][xval] = 0
 
 	cv2.imshow('image',img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+
+def splitImage(img, seperators):
+	images = []
+
+	height, width = img.shape
+	
+	if len(seperators) > 0:
+		images.append(img[0:height-1,0:seperators[0]])
+		
+		for i in range (0,len(seperators)-2):
+			images.append(img[0:height-1,seperators[i]:seperators[i+1]])
+
+		images.append(img[0:height-1,seperators[len(seperators)-1]:width-1])
+	return images
+
+def showImages(images):
+	for img in images:
+		cv2.imshow('image',img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
 
 
 def drawDensities(h_pixel_density, v_pixel_density):
@@ -103,49 +159,34 @@ def drawDensities(h_pixel_density, v_pixel_density):
 	plt.title('Horizontal pixel density')
 	plt.show();
 
-"""
-minwidth = []
-print "vertical mean: ", v_mean 
-print "vertical stdv: ", v_stdd
-for i in range(0,len(vertical_pixel_density)):
-	val = vertical_pixel_density[i]
-	if (val < (v_mean - 2*v_stdd)) or (val > (v_mean + 2*v_stdd)):
-		minwidth.append(i)
-
-h_mean = np.mean(horizontal_pixel_density)
-h_stdd = np.std(horizontal_pixel_density)
-
-minheight = []
-print "vertical mean: ", h_mean 
-print "vertical stdv: ", h_stdd
-for i in range(0,len(horizontal_pixel_density)):
-	val = horizontal_pixel_density[i]
-	if (val < (h_mean - 2*h_stdd)) or (val > (h_mean + 2*h_stdd)):
-		minheight.append(i)
-
-subimg = img[minheight[0]:height-1,0:width-1]
-"""
-
 
 if __name__ == "__main__":
 	minBoxWidth = 75
 	threshold = 10
 	padding = 5
 
+	horizontal_density_threshold = 1500
+	vertical_density_threshold = 100 
+
 	img = cv2.imread('example.pgm',0)
 	img = binarize(img)
 	img = rotateImage(img)
 
+
 	h_pixel_density = calcHorPixelDensity(img)
 	v_pixel_density = calcVerPixelDensity(img)
-	seperators = CalcSeperators(v_pixel_density, minBoxWidth, threshold)
+	
+	img = cropImage(img,horizontal_density_threshold,vertical_density_threshold, h_pixel_density, v_pixel_density)
+	
+	h_pixel_density = calcHorPixelDensity(img)
+	v_pixel_density = calcVerPixelDensity(img)
+	
+	seperators = CalcSeperators(v_pixel_density, minBoxWidth, threshold,padding)
 
 	drawDensities(h_pixel_density, v_pixel_density)
 
 	drawSeperators(img, seperators, padding)
 
+	images = splitImage(img, seperators)
 
-
-
-
-
+	showImages(images)
